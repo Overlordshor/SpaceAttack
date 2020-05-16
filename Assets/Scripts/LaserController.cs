0282037bbbfc01957d668afecb24f9bd7aa60cff
Дмитрий Шаров <overlord.shor@gmail.com>
2020-05-16 17:18:33 +0300

    Удаление лазера, ушедшего за поле камеры

--- Assets/Scripts/LaserController.cs
+++ Assets/Scripts/LaserController.cs
@@ -12,5 +12,10 @@ public class LaserController : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.up * Time.deltaTime * speed);
+
+        if (transform.position.y >= 5.36f)
+        {
+            Destroy(gameObject, 0.1f);
+        }
     }
 }
