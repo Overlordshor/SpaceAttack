61aceeeec88b6ee9b5f99da6721245a3bf03903f
Дмитрий Шаров <overlord.shor@gmail.com>
2020-05-16 23:21:11 +0300

    Добавлен Coroutine с появлением врагов

---
+++ Assets/Scripts/UseCoroutines.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseCoroutines : MonoBehaviour
+{
+    public GameObject EnemyPrefub;
+    void Start()
+    {
+        StartCoroutine(CloneEnemyPrefub());
+    }
+
+    IEnumerator CloneEnemyPrefub()
+    {
+        while (true)
+        {
+            Instantiate(EnemyPrefub, new Vector3(Random.Range(-7.88f, 8.01f), 6.38f, 0), Quaternion.identity);
+            yield return new WaitForSeconds(2.5f);
+        }
+    }
+}
