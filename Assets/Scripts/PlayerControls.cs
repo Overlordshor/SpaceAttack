0a4936fde9e59257f227e3a48f5b7175b0f919e2
Дмитрий Шаров <overlord.shor@gmail.com>
2020-05-16 13:43:27 +0300

    Реализовано движение корабля

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerControls : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float Speed = 5;
+    private float horizontalInput;
+    private float verticalInput;
     void Start()
     {
-        
+        transform.position = new Vector3(0, 0, 0);
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        transform.Translate(Vector3.right * Time.deltaTime * Speed * horizontalInput);
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.up * Time.deltaTime * Speed * verticalInput);
+        verticalInput = Input.GetAxis("Vertical");
     }
 }
